8edd4b191ea582405ebe526c4f311613de003a8c
clayto-nr <claytonrennan01@gmail.com>
2024-10-02 13:23:32 -0300

    update go

--- api/index.go
+++ api/index.go
@@ -5,12 +5,13 @@ import (
     "net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
+// Função exportada que será chamada pelo Vercel
+func Handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Hello, World!")
 }
 
 func main() {
-    http.HandleFunc("/", helloHandler) // Roteia a requisição para a função helloHandler
+    http.HandleFunc("/", Handler) // Roteia a requisição para a função Handler
     fmt.Println("Servidor escutando na porta 8080...")
     err := http.ListenAndServe(":8080", nil) // Inicia o servidor na porta 8080
     if err != nil {
--- go.mod
+++ go.mod
@@ -1,9 +1,5 @@
-module example/hello
+module myapp
 
-go 1.23.2
+go 1.18
 
-require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-)
+require github.com/tbxark/g4vercel v0.0.4 // indirect
